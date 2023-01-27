10bab4435ff7c5a709565768f03445517fd1ece6
ryushi-masa <pomechan.0505@icloud.com>
2023-01-27 22:37:35 +0900

    自作ミドルウェアを作成してグローバルミドルウェアとして適用した

---
+++ app/Http/Middleware/HeaderDumper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface;
+
+final class HeaderDumper
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $this->logger->info(
+            'request',
+            [
+                'header' => strval($request->headers)
+            ]
+        );
+        $response = $next($request);
+        $this->logger->info(
+            'response',
+            [
+                'header' => strval($response->headers)
+            ]
+        );
+        return $response;
+    }
+}
